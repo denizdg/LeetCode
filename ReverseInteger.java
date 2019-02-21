ee83bdff559e28cf57219af249a671e1f0e79279
Deniz Demir Gaidemak <dgaidemak@gmail.com>
2019-02-21 12:00:17 -0500

    Create ReverseInteger.java

---
+++ ReverseInteger.java
@@ -0,0 +1,17 @@
+class Solution {
+    public static int reverse(int x) {
+
+		long result = 0;
+
+      		while (x != 0) {
+			result = result * 10 + x % 10;
+			x = x / 10;
+
+		}
+
+		if (result < Integer.MIN_VALUE || result > Integer.MAX_VALUE)
+			return 0;
+		else
+			return (int)result;
+	}
+}
