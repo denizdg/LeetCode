540a6a36c2ac6521a8e7152a7cf04f5c4ac004a1
Deniz Demir Gaidemak <dgaidemak@gmail.com>
2019-02-28 15:43:02 -0500

    Create ValidParentheses.java

---
+++ ValidParentheses.java
@@ -0,0 +1,31 @@
+class Solution {
+    public boolean isValid(String s) {
+        char[] chars = s.toCharArray();
+		Stack<Character> charStack = new Stack<Character>();
+
+		for (Character c : chars) {
+			switch (c) {
+			case '(':
+				charStack.push(')');
+				break;
+			case '[':
+				charStack.push(']');
+				break;
+			case '{':
+				charStack.push('}');
+				break;
+			default:
+				if (c == ' ')
+					break;
+				else {
+					if (charStack.isEmpty() || c != charStack.peek())
+						return false;
+					charStack.pop();
+				}
+			}
+
+		}
+
+		return charStack.isEmpty();
+    }
+}
