fb978a67baa92afffdf1c61680845a4fd3096db5
Deniz Demir Gaidemak <dgaidemak@gmail.com>
2019-03-06 14:09:06 -0500

    Create RemoveDuplicatesFromSortedArray.java

---
+++ RemoveDuplicatesFromSortedArray.java
@@ -0,0 +1,30 @@
+	// my version just found the length of the array without duplicates
+//	public static int removeDuplicates(int[] nums) {
+//
+//		int len = nums.length;
+//
+//		for (int i = 1; i < nums.length; i++) {
+//
+//			if (nums[i] == nums[i - 1])
+//				len -= 1;
+//		}
+//
+//		return len;
+//	}
+
+	// the version that LeetCode accepts. Change the first element with the not duplicated elements and after the length the rest of the array looks like junk.
+	public static int removeDuplicates(int[] nums) {
+
+		int i = 0;
+
+		for (int j = 1; j < nums.length; j++) {
+
+			if (nums[i] != nums[j]){
+				i++;
+				nums[i] = nums[j];
+			}
+				
+		}
+
+		return i + 1;
+	}
